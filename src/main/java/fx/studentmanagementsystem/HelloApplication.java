8bf384bae108d1c376f671faa4bf186c15f18a24
Smarika Sharma <smarikasharma89@gmail.com>
2024-06-12 18:08:55 +1000

    updated css for login

--- src/main/java/fx/studentmanagementsystem/HelloApplication.java
+++ src/main/java/fx/studentmanagementsystem/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("System");
         stage.setScene(scene);
         stage.show();
         stage.setMinHeight(545);
