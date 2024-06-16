ce4f3c69ce8f3d65218579b6de7b0ca217eaa9aa
Smarika Sharma <smarikasharma89@gmail.com>
2024-06-16 14:41:06 +1000

    Signup fxml complete.

--- src/main/java/fx/studentmanagementsystem/HelloApplication.java
+++ src/main/java/fx/studentmanagementsystem/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("chooseUser.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("StudentForm-Signup.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("System");
         stage.setScene(scene);
@@ -23,4 +23,5 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
 }
--- src/main/java/fx/studentmanagementsystem/controller/signupcontoller.java
+++ src/main/java/fx/studentmanagementsystem/controller/signupcontoller.java
@@ -1,4 +1,32 @@
 package fx.studentmanagementsystem.controller;
 
-public class signupcontoller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class signupcontoller implements Initializable {
+
+    @FXML
+    private Label gender;
+
+    @FXML
+    private ChoiceBox<String> chooseGender;
+    private String[] genders = {"Male", "Female", "Others"};
+
+    @FXML
+    private Label faculty;
+
+    @FXML
+    private ChoiceBox<String> chooseFaculty;
+    private String[] faculties = {"BIHM", "BBA", "BCS"};
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        chooseGender.getItems().addAll(genders);
+        chooseFaculty.getItems().addAll(faculties);
+    }
 }
