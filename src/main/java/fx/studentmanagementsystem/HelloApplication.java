76a1251e9a04e547dde54d78a9324cd4473b5676
Smarika Sharma <smarikasharma89@gmail.com>
2024-07-19 22:08:28 +1000

    Studentform-Signup width/height fix

--- src/main/java/fx/studentmanagementsystem/HelloApplication.java
+++ src/main/java/fx/studentmanagementsystem/HelloApplication.java
@@ -12,7 +12,7 @@ import java.util.Objects;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/Fxml/login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/Fxml/Student/StudentForm-Signup.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
 
         Image icon = new Image(Objects.requireNonNull(HelloApplication.class.getResourceAsStream("/images/icon.png")));
