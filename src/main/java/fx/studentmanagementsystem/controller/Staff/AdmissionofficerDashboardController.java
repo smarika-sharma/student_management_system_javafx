7b78a420206fd6cebb4ddcd5f21801156e703755
Neha Aryal <neha22013112@iimscollege.edu.np>
2024-07-20 12:39:10 +0545

    "AdmissionDashboard"

--- src/main/java/fx/studentmanagementsystem/controller/Staff/AdmissionofficerDashboardController.java
+++ src/main/java/fx/studentmanagementsystem/controller/Staff/AdmissionofficerDashboardController.java
@@ -2,11 +2,25 @@ package fx.studentmanagementsystem.controller.Staff;
 
 import fx.studentmanagementsystem.DialogsutilLogout.DialogsUtil;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.awt.*;
+import java.io.IOException;
 
 import static fx.studentmanagementsystem.Uses.changeScene;
 
 public class AdmissionofficerDashboardController {
 
+    @FXML
+    public Button studentForm;
+
+    // Student Form
+
+    @FXML
+    public void studentFormclicked(ActionEvent event) throws IOException {
+        changeScene(event, "/Fxml/Student/StudentForm.fxml", "StudentForm");
+    }
 
     // logout button
 
--- src/main/java/fx/studentmanagementsystem/controller/Student/StudentFormController.java
+++ src/main/java/fx/studentmanagementsystem/controller/Student/StudentFormController.java
@@ -1,4 +1,5 @@
 package fx.studentmanagementsystem.controller.Student;
 
 public class StudentFormController {
+     
 }
